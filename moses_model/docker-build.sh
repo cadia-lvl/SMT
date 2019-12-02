f5a853281a5e6716b20ec05e46ce8c662ba3222d
Haukur <haukurpalljonsson@gmail.com>
2019-12-02 11:57:33 +0000

    Docker compose and Moses model packaging.

--- moses_model/docker-build.sh
+++ moses_model/docker-build.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 set -euxo
-TAG=$1
-docker build -t haukurp/moses-lvl:$TAG $(dirname "$0")
-docker push haukurp/moses-lvl:$TAG
+MODEL_NAME=$1
+DOCKER_IMAGE="haukurp/moses-smt:$MODEL_NAME"
+THIS_DIR=$(dirname "$0")
+# Afrita líkanið yfir á núverandi vél.
+scp -r haukurpj@terra.hir.is:/work/haukurpj/"$MODEL_NAME"/binarised "$THIS_DIR"/trained_model
+# Laga skráarendingar.
+sed -i 's|work/.*/binarised|work|g' "$THIS_DIR"/trained_model/moses.ini
+docker build -t "$DOCKER_IMAGE" "$THIS_DIR"
+rm -rf "$THIS_DIR"/trained_model
+docker push "$DOCKER_IMAGE"

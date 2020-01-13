84590e87414d845b885ba9241b728a13e1450cfa
Haukur <haukurpalljonsson@gmail.com>
2020-01-13 13:25:30 +0000

    Timing translation

---
+++ moses_model/moses-time-translation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=moses-time-translation.sh
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time=1:00:00
+
+MODEL_NAME=en-is-mideind-v2
+time singularity run -B /work/haukurpj/$MODEL_NAME/binarised:/work/haukurpj/$MODEL_NAME/binarised docker://haukurp/moses-smt:is-en-improved /opt/moses/bin/moses -f /work/haukurpj/$MODEL_NAME/binarised/moses.ini < /work/haukurpj/$MODEL_NAME/binarised/parice-test-final.en >/work/haukurpj/$MODEL_NAME/binarised/parice-test-final.translated

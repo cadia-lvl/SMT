65d9b5a5ee6b7633541878a4b7eac780affa277e
Haukur <haukurpalljonsson@gmail.com>
2020-01-28 15:05:46 +0000

    More tuning of pipeline scripts.

--- moses_model/moses-pipeline.sh
+++ moses_model/moses-pipeline.sh
@@ -8,10 +8,10 @@ set -euxo
 
 dir_name=~/SMT/moses_model
 
-jid_prep=$(sbatch --partition=longrunning $dir_name/moses-prep.sh)
-jid_lm=$(sbatch --partition=longrunning --dependency=afterok:$jid_prep $dir_name/moses-lm.sh)
-jid_train=$(sbatch --partition=longrunning --dependency=afterok:$jid_lm $dir_name/moses-train.sh)
-jid_tune=$(sbatch --partition=longrunning --dependency=afterok:$jid_train $dir_name/moses-tune.sh)
-jid_binarise=$(sbatch --partition=longrunning --dependency=afterok:$jid_tune $dir_name/moses-binarise.sh)
-jid_evaluate=$(sbatch --partition=longrunning --dependency=afterok:$jid_binarise $dir_name/moses-evaluate.sh)
+jid_prep=$(sbatch --partition=longrunning $dir_name/moses-prep.sh | awk -F' ' '{print $4}')
+jid_lm=$(sbatch --partition=longrunning --dependency=afterok:$jid_prep $dir_name/moses-lm.sh | awk -F' ' '{print $4}')
+jid_train=$(sbatch --partition=longrunning --dependency=afterok:$jid_lm $dir_name/moses-train.sh | awk -F' ' '{print $4}')
+jid_tune=$(sbatch --partition=longrunning --dependency=afterok:$jid_train $dir_name/moses-tune.sh | awk -F' ' '{print $4}')
+jid_binarise=$(sbatch --partition=longrunning --dependency=afterok:$jid_tune $dir_name/moses-binarise.sh | awk -F' ' '{print $4}')
+jid_evaluate=$(sbatch --partition=longrunning --dependency=afterok:$jid_binarise $dir_name/moses-evaluate.sh | awk -F' ' '{print $4}')
 

05a383fea74cb023f04cb9d024a1929fecafe94e
Haukur Pall Jonsson <haukurpj@torpaq.hir.is>
2020-02-13 16:27:06 +0000

    Mapping done

--- preprocessing/map.py
+++ preprocessing/map.py
@@ -1,4 +1,3 @@
-from typing import List, Tuple, Dict
 import pathlib
 import logging
 from concurrent.futures import ProcessPoolExecutor
@@ -9,24 +8,11 @@ from functools import partial
 import click
 from tqdm import tqdm
 
+from src.util import (read_paracorpora_pickle,
+                      ParaCorpora, Sentence, EnrichedCorpus, EnrichedSentence)
+
 log = logging.getLogger()
 
-Sentence = List
-POS = List
-Lemma = List
-
-Corpus = List[Sentence]
-langs = ['en', 'is']
-
-EnrichedSentence = Tuple[Sentence, POS, Lemma]
-EnrichedCorpus = List[EnrichedSentence]
-ParaCorpora = Dict[str, Dict[str, EnrichedCorpus]]
-
-
-def read_paracorpora_pickle(path: str) -> ParaCorpora:
-    import pickle
-    with open(path, 'rb') as handle:
-        return pickle.load(handle)
 
 # URI = re.compile(r"((http(s)?:\/\/)|(www)|([-a-zA-Z0-9:%_\+.~#?&/=]+?@))+([-a-zA-Z0-9@:%_\+.~#?&/=]+)", re.IGNORECASE)
 # URI_SIMPLE = re.compile(r"([-a-zA-Z0-9@:%_\+.~#?&/=]+?)(\.is|\.com)", re.IGNORECASE)
@@ -74,21 +60,32 @@ def apply_regexps(sent: Sentence, regexps) -> Sentence:
     return sent
 
 
-def process_sent(sentence: EnrichedSentence, enriched: bool) -> str:
-    low_sent = low_norm(sentence[0])
-    reg_sent = apply_regexps(low_sent, second_reg)
-    if not enriched:
+def process_sent(sentence: EnrichedSentence, form: bool, pos: bool, lemma: bool) -> str:
+    if form:
+        low_sent = low_norm(sentence[0])
+        reg_sent = apply_regexps(low_sent, second_reg)
+    if form and not pos and not lemma:
         return " ".join(reg_sent) + "\n"
-    else:
+    elif not form and not pos and lemma:
+        return " ".join([f'{lemma}' for lemma in sentence[2]]) + "\n"
+    elif form and pos and lemma:
         assert len(sentence[0]) == len(sentence[1]) == len(sentence[2])
         return " ".join([f'{form}|{pos}|{lemma}' for form, pos, lemma in zip(reg_sent, sentence[1], sentence[2])]) + "\n"
+    else:
+        raise NotImplementedError(f'form={form}, pos={pos}, lemma={lemma}')
 
 
-def process_corpus(corpus: EnrichedCorpus, lang: str, p_out: pathlib.Path, threads=10, enriched=True):
+def process_corpus(corpus: EnrichedCorpus,
+                   lang: str,
+                   p_out: pathlib.Path,
+                   threads,
+                   form,
+                   pos,
+                   lemma):
     with p_out.open('w+') as f_out:
         with ProcessPoolExecutor(max_workers=threads) as executor:
             results = tqdm(executor.map(
-                partial(process_sent, enriched=enriched),
+                partial(process_sent, form=form, pos=pos, lemma=lemma),
                 corpus,
                 chunksize=1000))
             for result in results:
@@ -98,22 +95,39 @@ def process_corpus(corpus: EnrichedCorpus, lang: str, p_out: pathlib.Path, threa
 @click.command()
 @click.argument('pickle_file')
 @click.argument('output_dir')
-@click.option('--enriched', is_flag=True)
+@click.option('--lemma', is_flag=True)
+@click.option('--pos', is_flag=True)
+@click.option('--form', is_flag=True)
 @click.option('--lines', type=int)
 @click.option('--threads', type=int, default=1)
-def main(pickle_file, output_dir, enriched, lines, threads):
-    para_corpora = read_paracorpora_pickle(pickle_file)
+def write(pickle_file,
+          output_dir,
+          lemma,
+          pos,
+          form,
+          lines,
+          threads):
+    para_corpora: ParaCorpora = read_paracorpora_pickle(pickle_file)
     for lang in para_corpora.keys():
         log.info(f'Processing lang={lang}')
         for corpus_name in para_corpora[lang]:
+            corpus_path = pathlib.Path(corpus_name)
             corpus: EnrichedCorpus = para_corpora[lang][corpus_name]
-            target_file = pathlib.Path(output_dir).joinpath(f'{corpus_name}.{lang}')
+            target_file = pathlib.Path(output_dir).joinpath(
+                f'{corpus_path.stem}{".form" if form else ""}{".pos" if pos else ""}{".lemma" if lemma else ""}.{lang}')
             log.info(f'Processing corpus={corpus_name} to target={target_file}')
             if lines != 0:
                 corpus = corpus[:lines]
-            process_corpus(corpus, lang, target_file, threads, enriched)
+            process_corpus(corpus, lang, target_file, threads, form, pos, lemma)
 
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(write)
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
-    main()
+    cli()
--- preprocessing/map.sh
+++ preprocessing/map.sh
@@ -4,4 +4,4 @@ conda activate notebook
 
 PICKLE_FILE='/work/haukurpj/data/filtered/Parice1.0/filtered.pickle'
 OUTPUT_DIR='/work/haukurpj/data/mapped'
-python tools/map.py $PICKLE_FILE $OUTPUT_DIR --enriched --threads 20
+python "$PWD"/map.py write $PICKLE_FILE $OUTPUT_DIR --lemma --threads 20

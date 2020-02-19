ddd6ae34463f98282bb46a7d4bb825dddc006fac
Haukur Pall Jonsson <haukurpj@torpaq.hir.is>
2020-02-19 15:19:40 +0000

    Changes to training pipeline for factored model

--- moses_model/moses-lm.sh
+++ moses_model/moses-lm.sh
@@ -14,7 +14,8 @@ cat ${CLEAN_DATA}.${LANG_TO} $LM_EXTRA_DATA >$LM_DATA
 run_in_singularity ${MOSESDECODER}/bin/lmplz --order $LM_ORDER --temp_prefix $WORK_DIR/ -S 10G --discount_fallback <${LM_DATA} >${LM}.arpa
 run_in_singularity ${MOSESDECODER}/bin/build_binary -S 10G ${LM}.arpa ${LM}
 
-# LM evaluation
-for test_set in $TEST_SETS; do
-  run_in_singularity ${MOSESDECODER}/bin/query ${LM} <$TEST_INPUT-$test_set.$LANG_TO | tail -n 4 >$MODEL_RESULTS/$test_set.ppl
+# LM evaluation, we evaluate on 
+for test_set in $TEST_PPL; do
+  TEST_SET_NAME=$(basename $test_set)
+  run_in_singularity ${MOSESDECODER}/bin/query ${LM} <$test_set | tail -n 4 >$MODEL_RESULTS/$TEST_SET_NAME.ppl
 done
---
+++ moses_model/moses-train-factored.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=moses-train
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=10GB
+#SBATCH --time=6:00:00
+#SBATCH --output=/home/staff/haukurpj/%j-%x.out
+set -euxo
+
+source $1
+# Train Moses factored model
+mkdir -p ${BASE_DIR}
+run_in_singularity ${MOSESDECODER}/scripts/training/train-model.perl -root-dir $BASE_DIR \
+  -corpus $CLEAN_DATA \
+  -f $LANG_FROM \
+  -e $LANG_TO \
+  -alignment $ALIGNMENT -reordering $REORDERING \
+  -lm 0:${LM_ORDER}:${LM}:8 \
+  -translation-factors 0-0 \
+  -mgiza -mgiza-cpus "$THREADS" \
+  -parallel -sort-buffer-size "$MEMORY" -sort-batch-size 1021 \
+  -sort-compress gzip -sort-parallel "$THREADS" \
+  -cores "$THREADS" \
+  -external-bin-dir $MOSESDECODER_TOOLS
